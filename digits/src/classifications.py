fdeb249d0b3d174f6f22e43b58ca155973b8e0c9
Montana <montanawong@gmail.com>
2015-10-24 01:21:56 -0400

    added classifications

---
+++ digits/src/classifications.py
@@ -0,0 +1,20 @@
+#ALPHABET
+
+a = [
+"http://science.opposingviews.com/DM-Resize/photos.demandstudios.com/getty/article/81/210/78033946.jpg?w=600&h=600&keep_ratio=1",
+"https://www.google.com/search?q=a+in+sign+language&es_sm=122&source=lnms&tbm=isch&sa=X&ved=0CAcQ_AUoAWoVChMIleaXi6zayAIVhjomCh3i3w4w&biw=1745&bih=834#imgrc=lfTB8xn5k5DfbM%3A",
+"http://www.lifeprint.com/asl101/signjpegs/a/a.jpg",
+"http://previews.123rf.com/images/tootles/tootles0603/tootles060300040/344720-alphabet-letter-a-in-sign-language-for-deaf-Stock-Photo.jpg",
+"http://usercontent1.hubimg.com/8683028_f260.jpg"
+]
+
+
+
+
+applause = [		"http://www.churchleaders.com/wp-content/uploads/files/article_images/clap_or_not_937398406.jpg",
+"http://i.imgur.com/GxvFP6h.jpg",
+"http://i.imgur.com/LgXOHBL.jpg",
+"http://i.imgur.com/JKmWCEn.jpg",
+"http://i.imgur.com/iJkf0st.jpg",
+"http://i.imgur.com/Dx1EULd.jpg"
+]
--- digits/src/train.py
+++ digits/src/train.py
@@ -7,22 +7,20 @@ concept = ClarifaiCustomModel()
 # eg: url = "www.image.com/cat.jpg"
 
 #APPLAUSE
-url = "http://www.churchleaders.com/wp-content/uploads/files/article_images/clap_or_not_937398406.jpg
-"
-url2= "http://i3.mirror.co.uk/incoming/article4764746.ece/ALTERNATES/s615/Grumpy-Cat.jpg"
 
-url3="http://media3.popsugar-assets.com/files/2014/08/04/788/n/1922398/6159171bee954510_thumb_temp_image8451461407171002/i/Grumpy-Cat-Releases-Second-Book.jpg"
-
-
+from classifications import applause, a
 #REPLACE NELLY WITH what we want the neural net to predict when it sees images similar to the one in the url
-concept.positive(url, "nelly")
-concept.positive(url2, "nelly")
-concept.positive(url3, "nelly")
-#concept.negative(url, 'pet' )
-concept.train('nelly')
+
+for url in applause:
+	concept.positive(url, "applause")
+
+#for url in a:
+#	concept.negative(url, "applause")
+
+concept.train('applause')
 
 #this assigns the result of whether or not the predict was successful 
-result = concept.predict(url3, 'nelly')
+result = concept.predict(applause[0], 'applause')
 
 #output the prediction!
 print dumps(result)
